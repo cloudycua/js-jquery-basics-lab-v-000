19c1fb48623a828e5b032db99387623729e327f2
Christine Cua <cloudycua@gmail.com>
2019-02-06 18:10:11 +0000

    Automatically backed up by Learn

--- js/calc.js
+++ js/calc.js
@@ -8,7 +8,7 @@ var operater = $('#operater').val()
 
 function result() {
   $('#equals').on('click', function() {
-    $('#result').html(2.4)
+    $('#result').html(parseFloat($('#number1').val())
   })
 }
 
