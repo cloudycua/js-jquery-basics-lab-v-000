01c14dd1bfb0ad96ab6befe5561467e2cda4bffc
Christine Cua <cloudycua@gmail.com>
2019-02-06 17:38:58 +0000

    Automatically backed up by Learn

--- js/calc.js
+++ js/calc.js
@@ -1,34 +1,38 @@
-var num1
-var num2
-var operation
+$(document).ready(function(){
+  result()
+})
 
-function result() {
-  $('').on('click', function() {
-    var num1 = $('#number1').val()
-    var num2 = $('#number2').val()
-    var operation = $('#operation').val()
+  var num1
+  var num2
+  var operation
 
-    return $('#result').val(calculate())
-  })
-}
+  function result() {
+    $('').on('click', function() {
+      var num1 = $('#number1').val()
+      var num2 = $('#number2').val()
+      var operation = $('#operation').val()
 
-function calculate(num1, num2, operation) {
-  var result = 0
-
-  switch (operation) {
-    case "+":
-      result = num1 + num2;
-      break;
-    case "/":
-      result = num1 / num2;
-      break;
-    case "-":
-      result = num1 - num2;
-      break;
-    case "*"
-      result = num1 * num2;
-      break;
+      return $('#result').val(calculate())
+    })
   }
 
-  return result
-}
+  function calculate(num1, num2, operation) {
+    var result = 0
+
+    switch (operation) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "/":
+        result = num1 / num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*"
+        result = num1 * num2;
+        break;
+    }
+
+    return result
+  }
