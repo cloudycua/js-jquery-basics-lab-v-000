5247273bdec97bdc3d136f4fc913625971c4eef8
Christine Cua <cloudycua@gmail.com>
2019-02-06 17:28:29 +0000

    Automatically backed up by Learn

--- js/calc.js
+++ js/calc.js
@@ -8,22 +8,26 @@ function result() {
     var num2 = $('#number2').val()
     var operation = $('#operation').val()
     var result = 0
-
-    switch (operation) {
-      case "+":
-        result = num1 + num2;
-        break;
-      case "/":
-        result = num1 / num2;
-        break;
-      case "-":
-        result = num1 - num2;
-        break;
-      case "*"
-        result = num1 * num2;
-        break;
-      }
-
-    }
   })
+}
+
+function calculate(num1, num2, operation) {
+  var result = 0
+  
+  switch (operation) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*"
+      result = num1 * num2;
+      break;
+  }
+
+  return result
 }
